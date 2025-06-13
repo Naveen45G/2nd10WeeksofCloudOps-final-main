1adb2974201c2147035219f3a406619ad7cbb92b
Naveen45G <naveenbandi782@gmail.com>
2025-06-13 14:14:20 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,7 +1,8 @@
 // const API_BASE_URL = "http://localhost:86";
-// const API_BASE_URL = "http://veera.narni.co.in";
+const API_BASE_URL = "
+a459cd1275b464624a5274a117746c76-1996045809.ap-south-1.elb.amazonaws.com";
 // export default API_BASE_URL;
- const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
+ //const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 // export default API_BASE_URL;
 //const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
 export default API_BASE_URL;
